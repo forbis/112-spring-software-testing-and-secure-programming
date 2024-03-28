8c0c238a8a8a5ba4a205aff30ae061a6672bb845
forbis <32932288+forbis@users.noreply.github.com>
2024-03-28 12:28:50 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,13 +10,20 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    await page.click('button.DocSearch.DocSearch-Button');
     // Type into search box
+    await page.waitForSelector('#docsearch-input');
+    await page.type('#docsearch-input', 'chipi chipi chapa chapa',{delay: 1000});
     // Wait for search result
     // Get the `Docs` result section
+    await page.waitForSelector('#docsearch-item-5');
     // Click on first result in `Docs` section
+    await page.click('#docsearch-item-5');
     // Locate the title
+    let textSelector = await page.waitForSelector('h1');
+    let title = await textSelector.evaluate(element=> element.textContent);
     // Print the title
-
+    console.log(title);
     // Close the browser
     await browser.close();
 })();
